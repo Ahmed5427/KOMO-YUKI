e0a710d43a1b796b5cb56c2e6c4f9adcd742666a
Ahmed Fathy <eng.fathy998@gmail.com>
2026-01-08 13:12:29 +0200

    Adjust background position in Hero component

--- components/Hero.js
+++ components/Hero.js
@@ -17,7 +17,7 @@ export default function Hero() {
         className="hidden md:block absolute inset-0 bg-contain bg-no-repeat"
         style={{
           backgroundImage: 'url(/images/hero-storefront.png)',
-          backgroundPosition: 'center 95%'
+          backgroundPosition: 'center 99%'
         }}
       >
         {/* Overlay - lighter on desktop */}
